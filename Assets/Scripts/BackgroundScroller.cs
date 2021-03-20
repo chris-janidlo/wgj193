d017473a8c42e7f4b6542c9add004784ac29af80
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-20 17:26:52 -0400

    Shamelessly steal the scrolling background effect from pixel frog

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float ScrollSpeed, ScrollSize;
+
+    float y;
+
+    void Update ()
+    {
+        y = Mathf.Repeat(y + ScrollSpeed * Time.deltaTime, ScrollSize);
+        transform.localPosition = new Vector3(0, y, -transform.parent.position.z);
+    }
+}
