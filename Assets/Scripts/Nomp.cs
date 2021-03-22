c2249264109371fd9827afb50dab3b17683258f6
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-22 00:19:04 -0400

    Basic enemy plus player health and death

---
+++ Assets/Scripts/Nomp.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using crass;
+
+public class Nomp : MonoBehaviour
+{
+    public Vector2 PlannedDirection => dirVecs[plannedDir];
+
+    public float MoveDistance;
+    public TransitionableVector2 MoveTransition;
+
+    public Vector2 MoveCheckBoxDimensions;
+    public LayerMask MoveCheckLayerMask;
+    [Tooltip("0 is Up, 1 is Right, 2 is Down, 3 is Left")]
+    public List<SpriteRenderer> Indicators;
+
+    int plannedDir;
+    List<Vector2> dirVecs = new List<Vector2> { Vector2.up, Vector2.right, Vector2.down, Vector2.left };
+
+    void Start ()
+    {
+        MoveTransition.AttachMonoBehaviour(this);
+        MoveTransition.Value = transform.position;
+    }
+
+    void Update ()
+    {
+        transform.position = MoveTransition.Value;
+    }
+
+    public void ChooseDirection ()
+    {
+        List<int> dirInts = new List<int> { 0, 1, 2, 3 };
+        dirInts.Remove(plannedDir);
+        dirInts.ShuffleInPlace();
+
+        plannedDir = -1;
+
+        foreach (int dir in dirInts)
+        {
+            if (!Physics2D.BoxCast(transform.position, MoveCheckBoxDimensions, 0, dirVecs[dir], MoveDistance, MoveCheckLayerMask))
+            {
+                Indicators[dir].enabled = true;
+                plannedDir = dir;
+                break;
+            }
+        }
+    }
+
+    public void Move ()
+    {
+        if (plannedDir == -1) return;
+
+        MoveTransition.StartTransitionTo((Vector2) transform.position + PlannedDirection * MoveDistance);
+        Indicators[plannedDir].enabled = false;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms.BaseAtoms;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int MaxHealth;
+    public LayerMask EnemyLayers;
+
+    public IntVariable CurrentPlayerHealth;
+
+    void Start ()
+    {
+        CurrentPlayerHealth.Value = MaxHealth;
+    }
+
+    void OnCollisionEnter2D (Collision2D collision)
+    {
+        // if the colliding gameobject is in the enemy layermask. see http://answers.unity.com/answers/1137700/view.html
+        if (EnemyLayers == (EnemyLayers | (1 << collision.gameObject.layer)))
+        {
+            CurrentPlayerHealth.Value--;
+            
+            if (CurrentPlayerHealth.Value <= 0)
+            {
+                // TODO: animate
+                Destroy(gameObject);
+            }
+        }
+    }
+}
