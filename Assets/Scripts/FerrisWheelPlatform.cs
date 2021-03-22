b0d8314a2201cc1d77b6c93efc201d40dc80d7cc
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-22 16:11:10 -0400

    Rotating platform

---
+++ Assets/Scripts/FerrisWheelPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FerrisWheelPlatform : MonoBehaviour
+{
+    public Transform CenterPoint;
+    public float Radius, RotationSpeed;
+
+    float t;
+
+    void Update ()
+    {
+        t += RotationSpeed * Time.deltaTime;
+
+        // parametric clockwise circle equation
+        transform.position = new Vector2
+        (
+            Radius * Mathf.Cos(t) + CenterPoint.position.x,
+            -Radius * Mathf.Sin(t) + CenterPoint.position.y
+        );
+    }
+}
