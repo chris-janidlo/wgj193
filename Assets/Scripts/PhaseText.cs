ae1be60627ad47f69f3eda9c8267e3ffb74dd5e1
crass_sandwich <christopherjanidlo@gmail.com>
2021-03-24 13:36:17 -0400

    Add an additional canvas for everything that isn't a UI card. Start it off by adding some text to the top of the screen telling you what phase is happening

---
+++ Assets/Scripts/PhaseText.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using crass;
+
+public class PhaseText : MonoBehaviour
+{
+    public string BuildPhaseText, EnemyPhaseText, PlatformingPhaseText;
+    public float PlatformingPhaseTextUptime;
+    public TransitionableFloat TextFadeTransition;
+
+    public TextMeshProUGUI TextContainer;
+
+    void Start ()
+    {
+        TextFadeTransition.AttachMonoBehaviour(this);
+    }
+
+    void Update ()
+    {
+        TextContainer.alpha = TextFadeTransition.Value;
+    }
+
+    public void OnCurrentPhaseChanged (Phase newPhase)
+    {
+        switch (newPhase)
+        {
+            case Phase.Build:
+                TextFadeTransition.FlashFromTo(0, 1);
+                TextContainer.text = BuildPhaseText;
+                break;
+
+            case Phase.Enemy:
+                StartCoroutine(fadeToText(EnemyPhaseText));
+                break;
+
+            case Phase.Platforming:
+                StartCoroutine(platformingRoutine());
+                break;
+        }
+    }
+
+    IEnumerator fadeToText (string text)
+    {
+        TextFadeTransition.FlashFromTo(1, 0);
+        yield return new WaitWhile(() => TextFadeTransition.Transitioning);
+        TextContainer.text = text;
+        TextFadeTransition.FlashFromTo(0, 1);
+    }
+
+    IEnumerator platformingRoutine ()
+    {
+        yield return fadeToText(PlatformingPhaseText);
+        yield return new WaitForSeconds(PlatformingPhaseTextUptime);
+        TextFadeTransition.FlashFromTo(1, 0);
+    }
+}
